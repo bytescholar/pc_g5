b6714747496aeeffff693fd8f26ef8ab0259332a
bytescholar <grbrun@gmail.com>
2024-06-09 21:41:00 -0300

    Update softwares_adicionais.sh

--- softwares_adicionais.sh
+++ softwares_adicionais.sh
@@ -2,7 +2,7 @@
 
 # Instalação de softwares pós reinicialização
 
-sudo apt install -y pdfarranger drawing shotcut obs-studio codeblocks gscan2pdf handbrake neovim converseen virt-manager gnome-video-trimmer video-downloader obfuscate gimp pdfsam
+sudo apt install -y pdfarranger drawing shotcut obs-studio codeblocks gscan2pdf handbrake neovim converseen virt-manager gnome-video-trimmer video-downloader obfuscate gimp pdfsam guake sakura
 
 sudo flatpak install -y flathub org.gnome.design.Lorem
 sudo flatpak install -y flathub com.google.Chrome

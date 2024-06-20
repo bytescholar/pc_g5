cbd7a404d7a421d6ebfe6e43492483992857dcc9
bytescholar <grbrun@gmail.com>
2024-06-20 20:01:31 -0300

    Delete atualiza.sh

--- atualiza.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Todos os updates + remoção de pacotes e arquivos desnecessários
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autoclean
-sudo apt autopurge -y
-sudo flatpak update --force-remove --assumeyes

afb9ab2519277b96828159b877865284e0f9bfc9
bytescholar <grbrun@gmail.com>
2026-04-15 17:04:42 -0300

    Update caixa-warsaw.sh

--- caixa-warsaw.sh
+++ caixa-warsaw.sh
@@ -6,14 +6,14 @@ set -e
 # --- Remove completamente as versões anteriores do pacote / serviço warsaw --- #
 sudo systemctl stop warsaw || true
 sudo apt purge -y warsaw || true
-rm -rf ~/.config/warsaw
+sudo rm -rf /usr/local/bin/warsaw /etc/warsaw || true
+rm -rf ~/.config/warsaw || true
 
 # --- Atualização dos repositórios --- #
 sudo apt update
 
 # --- Instalação de dependências e pacotes auxiliares --- #
-sudo apt install -y libnss3-tools libcurl4t64 || true
-sudo apt install -y libglib2.0-0t64 libgtk2.0-0t64 libnss3 libxss1 wget || true
+sudo apt install -y libnss3-tools libcurl4 libglib2.0-0 libnss3 libxss1 wget ca-certificates || true
 
 # --- Download da versão mais recente do Módulo de Segurança --- #
 DOWNLOAD_URL="https://imagem.caixa.gov.br/banner/fgr/GBPCEFwr64.deb"
@@ -26,10 +26,11 @@ sudo dpkg -i /tmp/warsaw.deb || sudo apt install -f -y
 rm -f /tmp/warsaw.deb
 
 # --- Habilitação do serviço warsaw --- #
+sudo systemctl daemon-reload || true
 sudo systemctl enable warsaw || true
 sudo systemctl start warsaw || true
 
 # --- Verifica se serviço warsaw está rodando --- #
-sudo systemctl status warsaw
+sudo systemctl is-active warsaw
 
 echo "Instalação concluída!"
